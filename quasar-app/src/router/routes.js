74e994f24d86654e5afeff733612581c635642ac
akashmhatre <akash.mhatre.dev@gmail.com>
2025-12-11 17:29:11 +0530

    feat: Add Bot Animation UI page with ThreeJS and SVG integration.

--- quasar-app/src/router/routes.js
+++ quasar-app/src/router/routes.js
@@ -9,6 +9,7 @@ const routes = [
        { path: "/scrolly", component: () => import("pages/ScrollyImages.vue") },
        { path: "/scene", component: () => import("pages/SceneAnimation.vue") },
        { path: "/scroll", component: () => import("pages/ScrollAnimation.vue") },
+       { path: "/bot", component: () => import("pages/BotAnmi.vue") },
     ],
   },
 
